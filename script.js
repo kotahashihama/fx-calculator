79c6093b52fa914fca4bba8e1c33c29d3791a5aa
Kota Hashihama <kotahashihama@gmail.com>
2019-05-13 10:13:24 +0900

    Refactor variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currencyInfo = [
+const pairs = [
   {
     name: 'EURUSD',
     rate: 0,
@@ -6,23 +6,23 @@ const currencyInfo = [
   },
   {
     name: 'USDJPY',
-    rate: 110.148,
-    rateExpected: 110.148
+    rate: 0,
+    rateExpected: 0
   },
   {
     name: 'GBPUSD',
-    rate: 1.30377,
-    rateExpected: 1.30377
+    rate: 0,
+    rateExpected: 0
   },
   {
     name: 'AUDUSD',
-    rate: 0.70186,
-    rateExpected: 0.70186
+    rate: 0,
+    rateExpected: 0
   }
 ]
 
-const form = {
-  pair: currencyInfo[0].name,
+const positionForm = {
+  pair: pairs[0].name,
   action: 'buy',
   lot: 0.01,
   entryRate: 0
@@ -31,13 +31,14 @@ const form = {
 const app = new Vue({
   el: '#app',
   data: {
+    balance: 0,
     broker: 'overseas',
     leverage: {
       overseas: 1000,
       japan: 25
     },
-    balance: 0,
-    standardSizes: {
+
+    tradingUnits: {
       overseas: 100000,
       japan: 10000
     },
@@ -47,94 +48,94 @@ const app = new Vue({
     },
 
     positions: [],
-    items: null,
+    positionForm,
 
-    currencyInfo,
-    form
+    pairs,
+    pairsFromAPI: null
   },
   computed: {
     unrealizedValue: function () {
-      let totalUnrealizedValue = 0
+      let unrealizedValue = 0
 
       for (let i = 0; i < this.positions.length; i++) {
+        const USDJPY = this.pairs[1]
         const pair = this.positions[i].pair
-        const currencyInfo = this.currencyInfo.find(function (currencyInfoItem) {
-          return (currencyInfoItem.name === pair)
+        const pairInfo = this.pairs.find(function (pairInfo) {
+          return (pairInfo.name === pair)
         })
-        const usdJpyInfo = this.currencyInfo[1]
 
-        if (currencyInfo.name === 'USDJPY') {
+        if (pairInfo.name === USDJPY.name) {
           // ドル円の処理
           if (this.positions[i].action === 'buy') {
-            totalUnrealizedValue += (currencyInfo.rateExpected - this.positions[i].entryRate) * this.standardSizes[this.broker] * this.positions[i].lot
+            unrealizedValue += (pairInfo.rateExpected - this.positions[i].entryRate) * this.tradingUnits[this.broker] * this.positions[i].lot
           } else {
-            totalUnrealizedValue += -(currencyInfo.rateExpected - this.positions[i].entryRate) * this.standardSizes[this.broker] * this.positions[i].lot
+            unrealizedValue += -(pairInfo.rateExpected - this.positions[i].entryRate) * this.tradingUnits[this.broker] * this.positions[i].lot
           }
         } else {
           // それ以外の通貨ペアの処理
           if (this.positions[i].action === 'buy') {
-            totalUnrealizedValue += (currencyInfo.rateExpected - this.positions[i].entryRate) * this.standardSizes[this.broker] * this.positions[i].lot * usdJpyInfo.rate
+            unrealizedValue += (pairInfo.rateExpected - this.positions[i].entryRate) * this.tradingUnits[this.broker] * this.positions[i].lot * USDJPY.rate
           } else {
-            totalUnrealizedValue += -(currencyInfo.rateExpected - this.positions[i].entryRate) * this.standardSizes[this.broker] * this.positions[i].lot * usdJpyInfo.rate
+            unrealizedValue += -(pairInfo.rateExpected - this.positions[i].entryRate) * this.tradingUnits[this.broker] * this.positions[i].lot * USDJPY.rate
           }
         }
       }
 
-      return Math.round(totalUnrealizedValue)
+      return Math.round(unrealizedValue)
     },
     pips: function () {
-      let totalPips = 0
+      let pips = 0
 
       for (let i = 0; i < this.positions.length; i++) {
+        const USDJPY = this.pairs[1]
         const pair = this.positions[i].pair
-        const currencyInfo = this.currencyInfo.find(function (currencyInfoItem) {
-          return (currencyInfoItem.name === pair)
+        const pairInfo = this.pairs.find(function (pairInfo) {
+          return (pairInfo.name === pair)
         })
-        const usdJpyInfo = this.currencyInfo[1]
 
-        if (currencyInfo.name === 'USDJPY') {
+        if (pairInfo.name === USDJPY.name) {
           // ドル円の処理
           if (this.positions[i].action === 'buy') {
-            totalPips += (currencyInfo.rateExpected - this.positions[i].entryRate) * 100
+            pips += (pairInfo.rateExpected - this.positions[i].entryRate) * 100
           } else {
-            totalPips += -(currencyInfo.rateExpected - this.positions[i].entryRate) * 100
+            pips += -(pairInfo.rateExpected - this.positions[i].entryRate) * 100
           }
         } else {
           // それ以外の通貨ペアの処理
           if (this.positions[i].action === 'buy') {
-            totalPips += (currencyInfo.rateExpected - this.positions[i].entryRate) * 10000
+            pips += (pairInfo.rateExpected - this.positions[i].entryRate) * 10000
           } else {
-            totalPips += -(currencyInfo.rateExpected - this.positions[i].entryRate) * 10000
+            pips += -(pairInfo.rateExpected - this.positions[i].entryRate) * 10000
           }
         }
       }
 
-      return Math.round(totalPips * 10) / 10
+      return Math.round(pips * 10) / 10
     },
     equity: function () {
       return Number(this.balance) + this.unrealizedValue
     },
     necessaryMargin: function () {
-      let totalNecessaryMargin = 0
+      let necessaryMargin = 0
 
       for (let i = 0; i < this.positions.length; i++) {
+        const USDJPY = this.pairs[1]
         const pair = this.positions[i].pair
-        const currencyInfo = this.currencyInfo.find(function (currencyInfoItem) {
-          return (currencyInfoItem.name === pair)
+        const pairInfo = this.pairs.find(function (pairInfo) {
+          return (pairInfo.name === pair)
         })
-        const usdJpyInfo = this.currencyInfo[1]
 
-        if (currencyInfo.name === 'USDJPY') {
+        if (pairInfo.name === 'USDJPY') {
           // ドル円の処理
-          totalNecessaryMargin += usdJpyInfo.rate * this.standardSizes[this.broker] * this.positions[i].lot / this.leverage[this.broker]
+          necessaryMargin += USDJPY.rate * this.tradingUnits[this.broker] * this.positions[i].lot / this.leverage[this.broker]
         } else {
           // それ以外の通貨ペアの処理
-          totalNecessaryMargin += currencyInfo.rate * usdJpyInfo.rate * this.standardSizes[this.broker] * this.positions[i].lot / this.leverage[this.broker]
+          necessaryMargin += pairInfo.rate * USDJPY.rate * this.tradingUnits[this.broker] * this.positions[i].lot / this.leverage[this.broker]
         }
       }
       // 必要証拠金 = 為替レート × Lot数 ÷ レバレッジ比率
 
-      return Math.round(totalNecessaryMargin)
+      return Math.round(necessaryMargin)
     },
     marginLevel: function () {
       if (this.positions.length) {
@@ -146,19 +147,19 @@ const app = new Vue({
   },
   methods: {
     setEntryRate: function () {
-      const pair = this.form.pair
-      const currencyInfo = this.currencyInfo.find(function (currencyInfoItem) {
-        return (currencyInfoItem.name === pair)
+      const pair = this.positionForm.pair
+      const pairInfo = this.pairs.find(function (pairInfo) {
+        return (currency.name === pair)
       })
 
-      this.form.entryRate = currencyInfo.rate
+      this.positionForm.entryRate = pairInfo.rate
     },
     addPosition: function () {
       this.positions.push({
-        pair: this.form.pair,
-        action: this.form.action,
-        lot: this.form.lot,
-        entryRate: this.form.entryRate
+        pair: this.positionForm.pair,
+        action: this.positionForm.action,
+        lot: this.positionForm.lot,
+        entryRate: this.positionForm.entryRate
       })
     },
     deletePosition: function (index) {
@@ -167,37 +168,37 @@ const app = new Vue({
       }
     },
     getCurrentRates: function () {
-      for (let i = 0; i < this.currencyInfo.length; i++) {
-        const pair = this.currencyInfo[i].name
-        const aaa = this.currencyInfo[i].name.slice(0, 3)
+      for (let i = 0; i < this.pairs.length; i++) {
+        const pair = this.pairs[i].name
+        const keyCurrency = this.pairs[i].name.slice(0, 3)
 
         if (pair === 'USDJPY') {
-          this.currencyInfo[i].rate = Math.round(this.items.JPY * 1000) / 1000
-          this.currencyInfo[i].rateExpected = Math.round(this.items.JPY * 1000) / 1000
+          this.pairs[i].rate = Math.round(this.pairsFromAPI.JPY * 1000) / 1000
+          this.pairs[i].rateExpected = Math.round(this.pairsFromAPI.JPY * 1000) / 1000
         } else {
-          this.currencyInfo[i].rate = Math.round(1 / this.items[aaa] * 100000) / 100000
-          this.currencyInfo[i].rateExpected = Math.round(1 / this.items[aaa] * 100000) / 100000
+          this.pairs[i].rate = Math.round(1 / this.pairsFromAPI[keyCurrency] * 100000) / 100000
+          this.pairs[i].rateExpected = Math.round(1 / this.pairsFromAPI[keyCurrency] * 100000) / 100000
         }
       }
     },
     setCurrentRate: function (index) {
-      const pair = this.currencyInfo[index].name
-      const aaa = this.currencyInfo[index].name.slice(0, 3)
+      const pair = this.pairs[index].name
+      const keyCurrency = this.pairs[index].name.slice(0, 3)
 
       if (pair === 'USDJPY') {
-        this.currencyInfo[index].rate = Math.round(this.items.JPY * 1000) / 1000
+        this.pairs[index].rate = Math.round(this.pairsFromAPI.JPY * 1000) / 1000
       } else {
-        this.currencyInfo[index].rate = Math.round(1 / this.items[aaa] * 100000) / 100000
+        this.pairs[index].rate = Math.round(1 / this.pairsFromAPI[keyCurrency] * 100000) / 100000
       }
     },
     setCurrentRateExpected: function (index) {
-      const pair = this.currencyInfo[index].name
-      const aaa = this.currencyInfo[index].name.slice(0, 3)
+      const pair = this.pairs[index].name
+      const keyCurrency = this.pairs[index].name.slice(0, 3)
 
       if (pair === 'USDJPY') {
-        this.currencyInfo[index].rateExpected = Math.round(this.items.JPY * 1000) / 1000
+        this.pairs[index].rateExpected = Math.round(this.pairsFromAPI.JPY * 1000) / 1000
       } else {
-        this.currencyInfo[index].rateExpected = Math.round(1 / this.items[aaa] * 100000) / 100000
+        this.pairs[index].rateExpected = Math.round(1 / this.pairsFromAPI[keyCurrency] * 100000) / 100000
       }
     }
   },
@@ -209,9 +210,9 @@ const app = new Vue({
   mounted: function () {
     axios.get('https://api.ratesapi.io/api/latest?base=USD')
       .then(function (response) {
-        app.items = response.data.rates
+        app.pairsFromAPI = response.data.rates
         app.getCurrentRates()
-        app.form.entryRate = Math.round(1 / app.items['EUR'] * 100000) / 100000
+        app.positionForm.entryRate = Math.round(1 / app.pairsFromAPI['EUR'] * 100000) / 100000
       })
   }
 })
