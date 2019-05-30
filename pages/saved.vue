114e3a2c952233b22a790bf43e7c3401a7bb45d5
Kota Hashihama <kotahashihama@gmail.com>
2019-05-30 13:01:56 +0900

    Change Google login into Twitter login

--- pages/saved.vue
+++ pages/saved.vue
@@ -1,34 +1,38 @@
 <template>
   <v-container grid-list-lg>
     <v-layout column>
-      <v-flex xs12>
-        <div v-if="isWaiting">
-          <p>読み込み中</p>
-        </div>
-        <div v-else>
-          <div v-if="!isLogin">
+      <template v-if="isWaiting">
+        <v-flex>
+          <p>読み込み中...</p>
+        </v-flex>
+      </template>
+      <template v-else>
+        <template v-if="!isLogin">
+          <v-flex>
             <p>この機能を使うにはログインが必要です。</p>
-            <v-btn @click="googleLogin" color="primary">Googleでログイン</v-btn>
-          </div>
-          <div v-else>
-            <p>{{ user.email }}でログイン中</p>
-            <v-btn @click="logOut"></v-btn>
-          </div>
-        </div>
-      </v-flex>
+            <v-btn @click="twitterLogin" color="primary">Twitterでログイン</v-btn>
+          </v-flex>
+        </template>
+        <template v-else>
+          <v-flex>
+            <p>{{ user.displayName }}でログイン中</p>
+            <v-btn @click="logOut">ログアウト</v-btn>
+          </v-flex>
 
-      <v-flex xs12>
-        <v-card>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>タイトルタイトルタイトル</v-list-tile-title>
-                <v-list-tile-sub-title>2019/05/30</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card>
-      </v-flex>
+          <v-flex>
+            <v-card>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-content>
+                    <v-list-tile-title>タイトルタイトルタイトル</v-list-tile-title>
+                    <v-list-tile-sub-title>2019/05/30</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </template>
+      </template>
     </v-layout>
   </v-container>
 </template>
@@ -57,8 +61,8 @@ export default {
     });
   },
   methods: {
-    googleLogin() {
-      const provider = new firebase.auth.GoogleAuthProvider();
+    twitterLogin() {
+      const provider = new firebase.auth.TwitterAuthProvider();
       firebase.auth().signInWithRedirect(provider);
     },
     logOut() {
